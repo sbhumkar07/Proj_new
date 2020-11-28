765e7e0fe70d9675dd9505bf3a31e33411ff69c4
Sarvesh Bhumkar <sarvesh.17u484@viit.ac.in>
2020-11-28 21:07:21 +0530

    commit new 1 uncomment

--- new_core_app.py
+++ new_core_app.py
@@ -18,7 +18,7 @@ from tensorflow.keras.preprocessing import image
 import keras
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.applications.imagenet_utils import decode_predictions
+#from tensorflow.keras.applications.imagenet_utils import decode_predictions
 
 """
 def resize_image_tom(image):
@@ -224,4 +224,4 @@ def convert_results(string):
 
 
 if __name__=="__main__":
-  app.run("0.0.0.0",80, debug = False)
+  app.run("34.121.118.165",80, debug = False)
